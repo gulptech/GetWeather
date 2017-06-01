0275724164c2d9d7214024cab0639db1cc31dab2
Joseph Pilon <dpilon@gmail.com>
2017-06-01 11:58:09 -0400

    Changed the example content

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -4,13 +4,13 @@ var React = require('react');
 //   render: function() {
 //     return (
 //       <div>
-//         <h3>Examples Here</h3>
+//         <h3>Examples Here!</h3>
 //         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 //       </div>
 //     );
 //   }
 // });
 
-var Examples = (props) => <div><h3>Examples Here (ES6)</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>;
+var Examples = (props) => <div><h3>Examples Here (ES6 - Arrow)!</h3></div>;
 
 module.exports = Examples;
