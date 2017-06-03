7d2c7e62732b4580bda73ef0a52f5fbd059782e4
Joseph Pilon <dpilon@gmail.com>
2017-06-02 23:46:02 -0400

    Styled About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
 var React = require('react');
+var {Link} = require('react-router');
+
 var About = (props) => {
   return (
     <div>
-      <h3>About Us</h3>
-      <p>Welcome to the about page!</p>
+      <h1 className="text-center">About Us</h1>
+      <p>Here are some links for you to look at.</p>
+      <ul>
+        <li><Link to="https://facebook.github.io/react">ReactJS</Link></li>
+        <li><Link to="http://foundation.zurb.com">Foundation</Link></li>
+        <li><Link to="https://www.openweathermap.org">open Weather Map</Link></li>
+      </ul>
     </div>
   )
 };;
