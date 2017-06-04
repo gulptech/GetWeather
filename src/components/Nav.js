df5d0093c4e20b115b4e3d38a8bbeb16f32be05a
Joseph Pilon <dpilon@gmail.com>
2017-06-04 00:37:23 -0400

    Fixed links and Weather Search in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,11 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function(e) {
     e.preventDefault();
-    alert('Not yet functional');
+    var location = encodeURIComponent(this.refs.search.value);
+    if (location.length > 0) {
+      this.refs.search.value = "";
+      window.location.hash = '#/?location=' + location;
+    }
   },
   render: function () {
     return (
@@ -20,7 +24,7 @@ var Nav = React.createClass({
         <div className="top-bar-right">
           <form onSubmit={this.onSearch} >
           <ul className="menu">
-            <li><input type="search" placeholder="Search weather by city" /></li>
+            <li><input type="search" placeholder="Search weather by city" ref="search" /></li>
             <li><input type="submit" className="button" value="Get Weather" /></li>
           </ul>
         </form>
